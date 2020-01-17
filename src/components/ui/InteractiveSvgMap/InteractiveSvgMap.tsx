1d201f437a0885734ff2d332fc203f07f6d5c636
Petros <petros@vervesearch.com>
2020-01-17 12:58:59 +0000

    create click and hover func separately

--- src/components/ui/InteractiveSvgMap/InteractiveSvgMap.tsx
+++ src/components/ui/InteractiveSvgMap/InteractiveSvgMap.tsx
@@ -1,40 +1,117 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
+import * as forEach from 'lodash.foreach';
 export interface InteractiveSvgMapProps {
     className?: string;
-    mapId: string;
+    id: string;
 
+    //the svg element
     svgImg: any;
+
+    // whether is interactive or not
     isInteractive: boolean;
-    hoverableChild?: any;
-    hoverableParent?: any;
+
+    //type of event 
+    eventType: 'click' | 'mousemove' | 'click|mousemove';
+
+    //interactive element classnames
+    interChildCls?: string;
+    interParentId?: string;
+
+    //tooltip
+    tooltip?: any;
+
+    //styles
+    stylesMod: any;
+
+    //funcs
+    onStateChange: (e: any) => void;
+    customOnClick: (any) => any;
 }
 
 export interface InteractiveSvgMapState {
     hlgtLoc: string;
-    hContinent: string;
     isTooltipVisible: boolean;
     toolTipX: number;
     toolTipY: number;
+    actionInEl: boolean;
 }
 
 export class InteractiveSvgMap extends React.Component<InteractiveSvgMapProps, InteractiveSvgMapState>{
     el: HTMLDivElement;
-    hlgtLoc; // this will be updated with the state so it can be used outside of the component
+    hlgtLoc; // location id - this will be updated with the state so it can be used outside of the component
+    hlgtMap; // mapId - this will be updated with the state so it can be used outside of the component
+    tooltipStyles;
     constructor(p: InteractiveSvgMapProps) {
         super(p);
         this.state = {
             hlgtLoc: "",
-            hContinent: "",
             isTooltipVisible: false,
             toolTipX: 0,
-            toolTipY: 0
+            toolTipY: 0,
+            actionInEl: false
         }
     }
 
     componentDidMount() {
-        console.log(this.props);
+        if (this.props.eventType == 'click' && this.props.isInteractive) {
+            document.addEventListener(this.props.eventType, (ev) => this.handleOutOfBounds(ev, this.props.interParentId));
+        }
+
+        this.tooltipStyles = {
+            top: `${Math.round(this.state.toolTipY) - 50 - 10}px`,
+            left: `${Math.round(this.state.toolTipX) - 50 / 2}px`,
+        }
+    }
+
+    componentDidUpdate(prevProps: InteractiveSvgMapProps, prevState: InteractiveSvgMapState) {
+        if (this.props.isInteractive) {
+            if ((prevState.toolTipX !== this.state.toolTipX) || (prevState.toolTipY !== this.state.toolTipY)) {
+                this.tooltipStyles = {
+                    top: `${Math.round(this.state.toolTipY) - 50 - 10}px`,
+                    left: `${Math.round(this.state.toolTipX) - 50 / 2}px`,
+                }
+            }
+        }
+    }
+
+    handleClick = (ev: any) => {
+        if (this.props.isInteractive) {
+            // current target vars(parent)
+            let cTarget = ev.currentTarget as any;
+            let cClass = cTarget.nodeName !== "DIV" ? cTarget.className.baseVal : cTarget.className;
+            let cId = cTarget.id;
+
+            //target vars (trigger element)
+            let target = ev.target as any;
+            let tClass = target.nodeName !== "DIV" ? target.className.baseVal : target.className;
+            let tId = target.id;
+
+            //list of interactive elements
+            let interEls = document.getElementsByClassName(this.props.interChildCls);
+
+            let isValidLoc = tClass.indexOf(this.props.interChildCls) !== -1;
+            // let isContinent = tClass.indexOf("circles-svg") !== -1;
+
+            if (isValidLoc && this.state.hlgtLoc !== tId) {
+                this.props.onStateChange(tId); //pass current id to parent component
+                this.setState({
+                    hlgtLoc: tId
+                });
+
+                //apply styles for each element
+                forEach(interEls, item => {
+                    item.id !== tId ? item.style[this.props.stylesMod[this.props.eventType]["attr"]] = this.props.stylesMod[this.props.eventType]["valueDisabled"] : item.style[this.props.stylesMod[this.props.eventType]["attr"]] = this.props.stylesMod[this.props.eventType]["valueEnabled"];
+                });
+
+                if (this.props.tooltip) {
+                    this.showToolTip(ev.clientX, ev.clientY, this.props.interParentId);
+                }
+            } else {
+                this.props.onStateChange("");
+                this.clearEffect(ev);
+            }
+        }
+
     }
 
     handleHover = (ev: any) => {
@@ -49,95 +126,115 @@ export class InteractiveSvgMap extends React.Component<InteractiveSvgMapProps, I
             let tClass = target.nodeName !== "DIV" ? target.className.baseVal : target.className;
             let tId = target.id;
 
-            let isValidLoc = tClass.indexOf(this.props.hoverableChild) !== -1;
+            //list of interactive elements
+            let interEls = document.getElementsByClassName(this.props.interChildCls);
+
+            let isValidLoc = tClass.indexOf(this.props.interChildCls) !== -1;
             // let isContinent = tClass.indexOf("circles-svg") !== -1;
 
             if (isValidLoc) {
+                this.props.onStateChange(tId); //pass current id to parent component
                 this.setState({
                     hlgtLoc: tId
-                })
-            }
+                });
 
+                //apply styles for each element
+                forEach(interEls, item => {
+                    item.id !== tId ? item.style[this.props.stylesMod[this.props.eventType]["attr"]] = this.props.stylesMod[this.props.eventType]["valueDisabled"] : item.style[this.props.stylesMod[this.props.eventType]["attr"]] = this.props.stylesMod[this.props.eventType]["valueEnabled"];
+                });
+
+                if (this.props.tooltip) {
+                    this.showToolTip(ev.clientX, ev.clientY, this.props.interParentId);
+                }
+            } else {
+                this.props.onStateChange("");
+                this.clearEffect(ev);
+            }
         }
     }
 
+    handleBoth = (ev) => {
 
+    }
+
+    // Check if event happened inside the parent element or outside
+    handleOutOfBounds = (ev, elId: string) => {
+        if (this.props.isInteractive) {
+            var svgMap = document.getElementById(elId);
+            var actionInEl = svgMap.contains(ev.target) && (svgMap !== ev.target);
+
+            this.setState({
+                actionInEl
+            })
+
+            if (!actionInEl) {
+                this.clearEffect(ev);
+            }
+        }
+    }
+
+    showToolTip = (x, y, parentId) => {
+        let parentTop = document.getElementById(parentId).getBoundingClientRect().top;
+        let parentLeft = document.getElementById(parentId).getBoundingClientRect().left;
+        let top = y - parentTop;
+        let left = x - parentLeft;
+
+        this.setState({
+            isTooltipVisible: true,
+            toolTipX: left,
+            toolTipY: top
+        })
+    }
+
+    clearEffect = (ev, eventType?) => {
+        if (this.props.isInteractive) {
+            var evType = eventType ? eventType : this.props.eventType;
+
+            this.setState({
+                hlgtLoc: "",
+                isTooltipVisible: false
+            })
+
+            forEach(document.getElementsByClassName(this.props.interChildCls), item => (
+                item.style[this.props.stylesMod[evType]["attr"]] = this.props.stylesMod[evType]["valueDefault"]
+            ))
+        }
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener(this.props.eventType, (ev) => this.handleOutOfBounds(ev, this.props.interParentId));
+    }
 
     render() {
         const { props, state } = this,
             cls = this.props.className || "";
 
-        console.log(state.hlgtLoc)
         return (
             <div
-                id={props.mapId}
+                id={props.id}
                 className={"interactive-svg-map " + cls}
                 ref={e => this.el = e}
-                onClick={this.handleHover}
-            // onMouseMove={}
-            // onMouseOut={}
+                onClick={props.eventType == 'click' ? (ev) => this.handleClick(ev) : props.eventType == 'click|mousemove' ? (ev) => this.handleBoth(ev) : null}
+                onMouseMove={props.eventType == 'mousemove' ? (ev) => this.handleHover(ev) : props.eventType == 'click|mousemove' ? (ev) => this.handleBoth(ev) : null}
+                onMouseOut={props.eventType == 'mousemove' || props.eventType == 'click|mousemove' ? (ev) => this.clearEffect(ev) : null}
             >
                 {
                     props.svgImg
                 }
-
+                {/* tooltip */}
+                {
+                    props.tooltip ?
+                        <div
+                            className={"map-tooltip " + cls + (!state.isTooltipVisible ? "hide" : "map-tooltip--shown")}
+                            style={this.tooltipStyles}
+                        >
+                            {props.tooltip}
+                        </div>
+                        :
+                        ""
+                }
                 <p>{state.hlgtLoc}</p>
-            </div>
+            </div >
         )
     }
 }
-
-
-// highlightCountry = (ev: React.SyntheticEvent<any> | any) => {
-//     if (SCREEN_WIDTH.IS_LARGE()) {
-//         //current target vars (parent)
-//         let cTarget = ev.currentTarget as any;
-//         let cClass = cTarget.nodeName !== "DIV" ? cTarget.className.baseVal : cTarget.className;
-//         let cId = cTarget.id;
-
-//         //target vars (trigger element)
-//         let target = ev.target as any;
-//         let tClass = target.nodeName !== "DIV" ? target.className.baseVal : target.className;
-//         let tId = target.id;
-
-//         let isCountry = tClass.indexOf("country-circle") !== -1;
-//         let isContinent = tClass.indexOf("circles-svg") !== -1;
-
-//         if (isCountry) {
-//             this.setState({
-//                 hCountry: tId,
-//                 hContinent: cId
-//             });
-
-//             forEach(document.getElementsByClassName(`country-circle`), item => {
-//                 item.id !== tId ? item.style.opacity = 0.4 : item.style.opacity = 1;
-//             })
-
-//             this.showToolTip(ev.clientX, ev.clientY, "map-image");
-//         }
-
-//         if (isContinent) {
-//             this.setState({
-//                 hCountry: "",
-//                 hContinent: cId
-//             });
-//         }
-
-//         forEach(document.getElementsByClassName("circle-svg"), item => (
-//             item.style.opacity = "0.8 !important"
-//         ))
-//     }
-// }
-
-// showToolTip = (x, y, parentId) => {
-//     let parentTop = document.getElementById(parentId).getBoundingClientRect().top;
-//     let parentLeft = document.getElementById(parentId).getBoundingClientRect().left;
-//     let top = y - parentTop;
-//     let left = x - parentLeft;
-
-//     this.setState({
-//         isTooltipVisible: true,
-//         toolTipX: left,
-//         toolTipY: top
-//     })
-// }
